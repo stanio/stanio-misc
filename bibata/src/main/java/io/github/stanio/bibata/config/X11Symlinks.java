69503ba05d2cdee859ae8ac4870b87ca76c575e7
Stanimir Stamenkov <stanio@yahoo.com>
2024-03-15 12:06:09 +0200

    X11Symlinks: Produce more detailed report of existing files/links
    Detect possible misconfigurations.

--- bibata/src/main/java/io/github/stanio/bibata/config/X11Symlinks.java
+++ bibata/src/main/java/io/github/stanio/bibata/config/X11Symlinks.java
@@ -13,8 +13,10 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.LinkOption;
 import java.nio.file.Path;
-import java.util.List;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 import java.util.stream.Stream;
 
 import com.google.gson.Gson;
@@ -22,7 +24,17 @@ import com.google.gson.reflect.TypeToken;
 
 public class X11Symlinks {
 
-    private Map<String, List<String>> symlinks;
+    // https://en.wikipedia.org/wiki/ANSI_escape_code#Colors
+    private static final String DIM_GRAY = "2;37";
+    private static final String BOLD_CYAN = "1;36";
+    private static final String BOLD_RED = "1;31";
+    private static final String YELLOW = "33";
+
+    private static final boolean plainTerm = Boolean.getBoolean("bibata.plainTerm");
+
+    private Map<String, Set<String>> symlinks;
+    private Map<String, String> backlinks;
+    private Set<String> cursorNames;
 
     public X11Symlinks() {
         try {
@@ -36,35 +48,117 @@ public class X11Symlinks {
         loadSymlinks(symlinks);
     }
 
-    private void loadSymlinks(URL symlinks) throws IOException {
-        try (InputStream in = symlinks.openStream();
+    private void loadSymlinks(URL symlinksConfig) throws IOException {
+        try (InputStream in = symlinksConfig.openStream();
                 Reader reader = new InputStreamReader(in, StandardCharsets.UTF_8)) {
-            this.symlinks = new Gson().fromJson(reader, new TypeToken<>() {/*RHS*/});
+            symlinks = new Gson().fromJson(reader, new TypeToken<>() {/*RHS*/});
+        }
+
+        backlinks = new HashMap<>();
+        cursorNames = new HashSet<>();
+        for (var entry : symlinks.entrySet()) {
+            String target = entry.getKey();
+            if (!cursorNames.add(target)) {
+                System.err.append("Conflicting target/link name: ").println(target);
+            }
+            if (entry.getValue() == null) continue;
+
+            entry.getValue().forEach(link -> {
+                String existing = backlinks.put(link, target);
+                if (existing != null) {
+                    System.err.append("Conflicting link: ")
+                              .append(link).append(" -> ")
+                              .println(String.join(", ", existing, target));
+                }
+                if (!cursorNames.add(link)) {
+                    System.err.append("Conflicting link/target name: ").println(link);
+                }
+            });
         }
     }
 
     public void create(Path baseDir) throws IOException {
-        try (Stream<Path> deepList = Files.walk(baseDir)) {
-            Iterable<Path> fileList = () -> deepList
-                    .filter(path -> Files.isRegularFile(path, LinkOption.NOFOLLOW_LINKS))
-                    .iterator();
-            for (Path file : fileList) {
-                Path fileName = file.getFileName();
-                List<String> x11Links = symlinks.get(fileName.toString());
-                if (x11Links == null) continue;
+        try (Stream<Path> dirStream = Files.walk(baseDir)) {
+            Iterable<Path> deepList = () -> dirStream.iterator();
+            for (Path path : deepList) {
+                Path fileName = path.getFileName();
+                String name = fileName.toString();
+                if (!cursorNames.contains(name)) {
+                    if (!Files.isDirectory(path)) {
+                        System.out.println(withColor(
+                                relativize(path, baseDir), DIM_GRAY));
+                    }
+                    continue;
+                }
 
-                System.out.print(file);
+                System.out.print(relativize(path, baseDir));
+                if (Files.isDirectory(path)) {
+                    System.out.println(withColor(" (directory!)", YELLOW));
+                    continue;
+                }
+
+                if (Files.isSymbolicLink(path)) {
+                    printLinkInfo(path, baseDir);
+                    continue;
+                }
+
+                Set<String> x11Links = symlinks.get(fileName.toString());
+                if (x11Links == null || x11Links.isEmpty()) {
+                    if (!symlinks.containsKey(fileName.toString())) {
+                        System.out.print(withColor(" (=/=> "
+                                + backlinks.get(fileName.toString()) + ")", YELLOW));
+                    }
+                    System.out.println();
+                    continue;
+                }
+
                 for (String linkName : x11Links) {
-                    Path link = file.resolveSibling(linkName);
+                    Path link = path.resolveSibling(linkName);
                     if (Files.notExists(link, LinkOption.NOFOLLOW_LINKS)) {
                         Files.createSymbolicLink(link, fileName);
                         System.out.println();
-                        System.out.append("\t<- ").print(link.getFileName());
+                        System.out.append('\t').print(withColor(
+                                "<- " + link.getFileName(), BOLD_CYAN));
                     }
                 }
                 System.out.println();
             }
         }
+    }
+
+    private void printLinkInfo(Path file, Path baseDir) throws IOException {
+        String linkName = file.getFileName().toString();
+        String actualTarget = Files.readSymbolicLink(file).toString();
+        System.out.append(" -> ");
+        if (Files.exists(file.resolveSibling(actualTarget), LinkOption.NOFOLLOW_LINKS)) {
+            System.out.print(actualTarget);
+        } else {
+            System.out.print(withColor(actualTarget, BOLD_RED));
+        }
+
+        String configuredTarget;
+        if (symlinks.containsKey(linkName)) {
+            System.out.println(withColor(" (=/=>)", YELLOW));
+        } else if (actualTarget.equals(configuredTarget = backlinks.get(linkName))) {
+            System.out.println();
+        } else { // actualTarget != configuredTarget
+            assert (configuredTarget != null);
+            System.out.println(withColor(" (=/=> " + configuredTarget + ")", YELLOW));
+        }
+    }
+
+    private static Path relativize(Path path, Path base) {
+        Path context = base.getParent();
+        if (context == null) {
+            context = base;
+        }
+        return context.relativize(path);
+    }
+
+    private static String withColor(Object val, String color) {
+        // https://en.wikipedia.org/wiki/ANSI_escape_code#SGR_(Select_Graphic_Rendition)_parameters
+        return plainTerm ? String.valueOf(val)
+                         : "\u001B[" + color + "m" + val + "\u001B[0m";
     }
 
     public static void main(String[] args) throws Exception {
@@ -75,7 +169,12 @@ public class X11Symlinks {
 
         X11Symlinks links = new X11Symlinks();
         for (String str : args) {
-            links.create(Path.of(str));
+            try {
+                links.create(Path.of(str));
+            } catch (Exception e) {
+                System.err.println();
+                throw e;
+            }
         }
     }
 
