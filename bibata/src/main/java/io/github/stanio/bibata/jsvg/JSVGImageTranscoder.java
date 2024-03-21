c854e333a59be54bb0d30520ef40c8e4c01068f2
Stanimir Stamenkov <stanio@yahoo.com>
2024-03-21 11:09:52 +0200

    JSVGImageTranscoder: Move its dependencies as nested classes
    Prepare to move into the JSVGRendererBackend source file.

--- bibata/src/main/java/io/github/stanio/bibata/jsvg/JSVGImageTranscoder.java
+++ bibata/src/main/java/io/github/stanio/bibata/jsvg/JSVGImageTranscoder.java
@@ -4,15 +4,27 @@
  */
 package io.github.stanio.bibata.jsvg;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Iterator;
+import java.util.Objects;
 import java.util.Optional;
 
+import javax.xml.XMLConstants;
+import javax.xml.stream.XMLEventReader;
+import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.events.XMLEvent;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stax.StAXResult;
 
 import org.w3c.dom.Document;
 
@@ -29,12 +41,16 @@ import javax.imageio.stream.MemoryCacheImageOutputStream;
 import com.github.weisj.jsvg.SVGDocument;
 import com.github.weisj.jsvg.SVGRenderingHints;
 import com.github.weisj.jsvg.parser.DefaultParserProvider;
+import com.github.weisj.jsvg.parser.NodeSupplier;
 import com.github.weisj.jsvg.parser.StaxSVGLoader;
 import com.github.weisj.jsvg.parser.SynchronousResourceLoader;
 
 import com.jhlabs.image.ShadowFilter;
 
 import io.github.stanio.bibata.svg.DropShadow;
+import io.github.stanio.bibata.util.XMLEventBufferReader;
+import io.github.stanio.bibata.util.XMLEventBufferWriter;
+import io.github.stanio.bibata.util.XMLInputFactoryAdapter;
 
 /**
  * Mimics the (Batik) DynamicImageTranscoder API, partially.
@@ -169,4 +185,130 @@ public class JSVGImageTranscoder {
         return writer;
     }
 
-}
+
+    interface DOMInput {
+
+        Document document();
+
+        default DOMSource asDOMSource() {
+            return new DOMSource(document());
+        }
+
+        @SuppressWarnings("unchecked")
+        static <T extends InputStream & DOMInput> T fakeStream(Document source) {
+            return (T) new DOMInputFakeStream(source);
+        }
+
+        static StaxSVGLoader newSVGLoader() {
+            return DOMSourceInputFactory.newSVGLoader();
+        }
+
+    } // interface DOMInput
+
+
+    static final class DOMInputFakeStream
+            extends ByteArrayInputStream implements DOMInput {
+
+        private static final byte[] EMPTY = new byte[0];
+
+        private final Document document;
+
+        DOMInputFakeStream(Document document) {
+            super(EMPTY);
+            this.document = Objects.requireNonNull(document);
+        }
+
+        @Override
+        public Document document() {
+            return document;
+        }
+
+    } // class DOMInputFakeStream
+
+
+    /**
+     * @see  DOMInput
+     */
+    static class DOMSourceInputFactory extends XMLInputFactoryAdapter {
+
+        // Can be reused after close()
+        private static final ByteArrayInputStream
+                EMPTY_ENTITY = new ByteArrayInputStream(new byte[0]);
+
+        private static final
+        ThreadLocal<Transformer> localTransformer = ThreadLocal.withInitial(() -> {
+            try {
+                TransformerFactory tf = TransformerFactory.newInstance();
+                tf.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
+                return tf.newTransformer();
+            } catch (TransformerConfigurationException e) {
+                throw new IllegalStateException(e);
+            }
+        });
+
+        private final XMLInputFactory defaultDelegate;
+
+        DOMSourceInputFactory() {
+            defaultDelegate = XMLInputFactory.newInstance();
+            defaultDelegate.setXMLResolver(
+                    (publicID, systemID, baseURI, namespace) -> EMPTY_ENTITY);
+        }
+
+        @Override
+        protected XMLInputFactory delegate() {
+            return defaultDelegate;
+        }
+
+        protected final XMLEventReader createReader(DOMInput input)
+                throws XMLStreamException {
+            try {
+                return super.createXMLEventReader(input.asDOMSource());
+            } catch (UnsupportedClassVersionError e) {
+                // REVISIT: Implement EventBufferReader/Writer as piped streams,
+                // if we want to allow for large document processing.  Alternatively,
+                // EventBufferWriter.eventIterator() should produce events on demand,
+                // that could also happen async with some read-ahead buffering.
+                return new XMLEventBufferReader(xmlEventsFor(input.document()));
+            }
+        }
+
+        private static Iterable<XMLEvent> xmlEventsFor(Document document) {
+            XMLEventBufferWriter bufferWriter = new XMLEventBufferWriter();
+            try {
+                localTransformer.get()
+                        .transform(new DOMSource(document),
+                                   new StAXResult(bufferWriter));
+            } catch (TransformerException e) {
+                throw new IllegalStateException(e);
+            }
+            return bufferWriter.getBuffer();
+        }
+
+        @Override
+        public XMLEventReader createXMLEventReader(InputStream stream)
+                throws XMLStreamException {
+            if (stream instanceof DOMInput) {
+                return createReader((DOMInput) stream);
+            }
+            return super.createXMLEventReader(stream);
+        }
+
+        @Override
+        public XMLEventReader createXMLEventReader(InputStream stream, String encoding)
+                throws XMLStreamException {
+            if (stream instanceof DOMInput) {
+                return createReader((DOMInput) stream);
+            }
+            return super.createXMLEventReader(stream, encoding);
+        }
+
+        private static final NodeSupplier NODE_SUPPLIER = new NodeSupplier();
+
+        public static StaxSVGLoader newSVGLoader() {
+            return new StaxSVGLoader(NODE_SUPPLIER, new DOMSourceInputFactory());
+        }
+
+    } // class DOMSourceInputFactory
+
+
+} // class JSVGImageTranscoder
