59a457081fcacb5a8f34fcbe524b689372656d0b
Stanimir Stamenkov <stanio@yahoo.com>
2025-08-17 17:34:54 +0300

    SmoothDownscale: Gamma-correct scaling
    This is likely very expensive - see if it could be parametrized.  Involves
    numerous conversions, including workarounds for what it seems Java2D bugs.

---
+++ src/test/java/io/github/stanio/awt/SmoothDownscaleTest.java
@@ -0,0 +1,40 @@
+/*
+ * SPDX-FileCopyrightText: 2025 Stanio <stanio AT yahoo DOT com>
+ * SPDX-License-Identifier: 0BSD
+ */
+package io.github.stanio.awt;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.awt.Point;
+import java.awt.image.BufferedImage;
+
+import javax.imageio.ImageIO;
+
+import org.junit.jupiter.api.Test;
+
+public class SmoothDownscaleTest {
+
+    @Test
+    void gammaCorrectScaling() throws Exception {
+        BufferedImage source = ImageIO
+                .read(SmoothDownscaleTest.class.getResource("gamma-1.0-or-2.2.png"));
+
+        BufferedImage result = SmoothDownscale
+                .resize(source, source.getWidth() / 2, source.getHeight() / 2);
+
+        assertThat(result).isNotNull();
+    }
+
+    @Test
+    void alignPixelAnchor() throws Exception {
+        BufferedImage source = ImageIO
+                .read(SmoothDownscaleTest.class.getResource("scale-align.png"));
+
+        BufferedImage result = SmoothDownscale.resize(source, 23, 23, new Point(13, 13));
+
+        assertThat(result).isNotNull();
+    }
+
+
+}
