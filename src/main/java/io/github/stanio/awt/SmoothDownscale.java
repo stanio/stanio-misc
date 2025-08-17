8aaf94c02dfca70dbda6587f8dadeeb90d9ee3e1
Stanimir Stamenkov <stanio@yahoo.com>
2025-08-17 17:05:49 +0300

    SmoothDownscale: Add support for pixel grid align anchor hint
    Similar to SVGSizing.alignToGrid().  As it is image-specific, possibly support
    reading this from an Image property.

--- src/main/java/io/github/stanio/awt/SmoothDownscale.java
+++ src/main/java/io/github/stanio/awt/SmoothDownscale.java
@@ -4,11 +4,10 @@
  */
 package io.github.stanio.awt;
 
-import java.util.Collections;
-import java.util.Map;
-
 import java.awt.Graphics2D;
+import java.awt.Point;
 import java.awt.RenderingHints;
+import java.awt.geom.Point2D;
 import java.awt.image.BufferedImage;
 
 /**
@@ -22,52 +21,109 @@ import java.awt.image.BufferedImage;
  */
 public final class SmoothDownscale {
 
+    public static class HintKey extends RenderingHints.Key {
+        public static final HintKey ALIGN = new HintKey(1);
+
+        private HintKey(int privateKey) {
+            super(privateKey);
+        }
+
+        @Override
+        public boolean isCompatibleValue(Object val) {
+            return (val instanceof Point);
+        }
+    }
+
+    private static final boolean DEBUG = false;
+
+    private static final RenderingHints defaultHints;
+    static {
+        RenderingHints hints = new RenderingHints(
+                RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+        hints.put(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BICUBIC);
+        hints.put(RenderingHints.KEY_ALPHA_INTERPOLATION, RenderingHints.VALUE_ALPHA_INTERPOLATION_QUALITY);
+        hints.put(RenderingHints.KEY_COLOR_RENDERING, RenderingHints.VALUE_COLOR_RENDER_QUALITY);
+        defaultHints = hints;
+    }
+
     public static BufferedImage resize(BufferedImage image,
                                        int targetWidth,
                                        int targetHeight) {
-        return resize(image, targetWidth, targetHeight, false);
+        return resize(image, targetWidth, targetHeight, defaultHints);
     }
 
     public static BufferedImage resize(BufferedImage image,
                                        int targetWidth,
                                        int targetHeight,
-                                       boolean bicubic) {
+                                       RenderingHints hints) {
+        Point alignAnchor = (hints == null) ? null : (Point) hints.get(HintKey.ALIGN);
+        if (alignAnchor != null) {
+            alignAnchor = new Point(alignAnchor);
+        }
+        return resize(image, targetWidth, targetHeight, hints, alignAnchor);
+    }
+
+    public static BufferedImage resize(BufferedImage image,
+                                       int targetWidth,
+                                       int targetHeight,
+                                       Point alignAnchor) {
         return resize(image, targetWidth, targetHeight,
-                Collections.singletonMap(RenderingHints.KEY_INTERPOLATION,
-                               bicubic ? RenderingHints.VALUE_INTERPOLATION_BICUBIC
-                                       : RenderingHints.VALUE_INTERPOLATION_BILINEAR));
+                defaultHints, alignAnchor == null ? null : new Point(alignAnchor));
     }
 
-    public static BufferedImage resize(BufferedImage image,
-                                       int targetWidth,
-                                       int targetHeight,
-                                       Map<RenderingHints.Key, ?> hints) {
+    private static BufferedImage resize(BufferedImage image,
+                                        int targetWidth,
+                                        int targetHeight,
+                                        RenderingHints hints,
+                                        Point alignAnchor) {
         int sourceWidth = image.getWidth();
         int sourceHeight = image.getHeight();
-        int doubleWidth = targetWidth * 2;
-        int doubleHeight = targetHeight * 2;
-
         BufferedImage source = image;
-        if (doubleWidth < sourceWidth
-                || doubleHeight < sourceHeight) {
-            int w = doubleWidth < sourceWidth ? doubleWidth : targetWidth;
-            int h = doubleHeight < sourceHeight ? doubleHeight : targetHeight;
-            source = resize(source, w, h, hints);
+        {
+            int doubleWidth = targetWidth * 2;
+            int doubleHeight = targetHeight * 2;
+            if (doubleWidth < sourceWidth
+                    || doubleHeight < sourceHeight) {
+                int tempWidth = doubleWidth < sourceWidth ? doubleWidth : targetWidth;
+                int tempHeight = doubleHeight < sourceHeight ? doubleHeight : targetHeight;
+                source = resize(source, tempWidth, tempHeight, hints, alignAnchor);
+                sourceWidth = source.getWidth();
+                sourceHeight = source.getHeight();
+            }
         }
 
-        int imageType = source.getColorModel().hasAlpha()
-                        ? BufferedImage.TYPE_INT_ARGB
-                        : BufferedImage.TYPE_INT_RGB;
-        BufferedImage scaled =
-                new BufferedImage(targetWidth, targetHeight, imageType);
+        BufferedImage scaled = new BufferedImage(targetWidth, targetHeight,
+                                                 source.getColorModel().hasAlpha()
+                                                 ? BufferedImage.TYPE_INT_ARGB
+                                                 : BufferedImage.TYPE_INT_RGB);
         Graphics2D g = scaled.createGraphics();
         try {
             g.addRenderingHints(hints);
-            g.drawImage(source, 0, 0, targetWidth, targetHeight, null);
+            if (alignAnchor == null) {
+                g.drawImage(source, 0, 0, targetWidth, targetHeight, null);
+            } else {
+                double scaleX = (double) targetWidth / sourceWidth;
+                double scaleY = (double) targetHeight / sourceHeight;
+                Point2D offset = alignToGrid(alignAnchor, scaleX, scaleY);
+                g.scale(scaleX, scaleY);
+                g.translate(offset.getX(), offset.getY());
+                g.drawImage(source, 0, 0, null);
+            }
         } finally {
             g.dispose();
         }
         return scaled;
+    }
+
+    private static Point2D alignToGrid(Point anchor, double scaleX, double scaleY) {
+        if (DEBUG) System.out.println(" -> " + anchor);
+        int targetX = (int) Math.round(anchor.x * scaleX);
+        int targetY = (int) Math.round(anchor.y * scaleY);
+        Point2D offset = new Point2D.Double(targetX / scaleX - anchor.x,
+                                            targetY / scaleY - anchor.y);
+        if (DEBUG) System.out.println(" <- " + offset);
+        anchor.setLocation(targetX, targetY);
+        return offset;
     }
 
     private SmoothDownscale() {/* no instances */}
