stanio/stanio-misc

commit a828636899f436d4e403ef179cc7dee644f7467d

SmoothDownscale: Reverse scaling strategy
First scale to a size that is integral factor of the target dimensions,
and is greater than or equal to half the source size.  Then perform as
many half downscale operations necessary to get to the target size.

Subjectively this produces better results, with more perceived detail
preserved.  It could be just me imagining, also.

Have SmoothDownscale.resize() produce the target size, already.

src/main/java/io/github/stanio/windows/Cursor.java
@@ -17,10 +17,8 @@ import java.util.List;
 import java.util.Optional;
 
 import java.awt.Dimension;
-import java.awt.Graphics2D;
 import java.awt.Point;
 import java.awt.Rectangle;
-import java.awt.RenderingHints;
 import java.awt.geom.AffineTransform;
 import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
@@ -149,19 +147,8 @@ public class Cursor {
             argb = image;
             hxy = clampHotspot(hotspot);
         } else {
-            argb = new BufferedImage(sizing.target.width,
-                                     sizing.target.height,
-                                     BufferedImage.TYPE_INT_ARGB);
-            Graphics2D g = argb.createGraphics();
-            g.setRenderingHint(RenderingHints.KEY_INTERPOLATION,
-                               RenderingHints.VALUE_INTERPOLATION_BILINEAR);
-            g.setRenderingHint(RenderingHints.KEY_ALPHA_INTERPOLATION,
-                               RenderingHints.VALUE_ALPHA_INTERPOLATION_QUALITY);
-
-            AffineTransform txf = sizing.getTransform();
-            g.drawRenderedImage(SmoothDownscale.prepare(image, txf), txf);
-            g.dispose();
-
+            argb = SmoothDownscale.resize(image,
+                    sizing.target.width, sizing.target.height);
             hxy = clampHotspot(sizing.transform.transform(hotspot, null));
         }
         addARGBImage(argb, hxy);

src/main/java/io/github/stanio/awt/SmoothDownscale.java
@@ -9,7 +9,6 @@ import java.util.Map;
 
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
-import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
 
 /**
@@ -23,77 +22,52 @@ import java.awt.image.BufferedImage;
  */
 public final class SmoothDownscale {
 
-    public static BufferedImage prepare(BufferedImage image,
-                                        int targetWidth,
-                                        int targetHeight) {
-        return prepare(image, targetWidth, targetHeight, false);
+    public static BufferedImage resize(BufferedImage image,
+                                       int targetWidth,
+                                       int targetHeight) {
+        return resize(image, targetWidth, targetHeight, false);
     }
 
-    public static BufferedImage prepare(BufferedImage image,
-                                        int targetWidth,
-                                        int targetHeight,
-                                        boolean bicubic) {
-        AffineTransform txf = new AffineTransform();
-        txf.scale(targetWidth / (double) image.getWidth(),
-                  targetHeight / (double) image.getHeight());
-        return prepare(image, txf, bicubic);
-    }
-
-    public static BufferedImage prepare(BufferedImage image,
-                                        AffineTransform transform) {
-        return prepare(image, transform, false);
-    }
-
-    public static BufferedImage prepare(BufferedImage image,
-                                        AffineTransform transform,
-                                        boolean bicubic) {
-        return prepare(image, transform,
+    public static BufferedImage resize(BufferedImage image,
+                                       int targetWidth,
+                                       int targetHeight,
+                                       boolean bicubic) {
+        return resize(image, targetWidth, targetHeight,
                 Collections.singletonMap(RenderingHints.KEY_INTERPOLATION,
                                bicubic ? RenderingHints.VALUE_INTERPOLATION_BICUBIC
                                        : RenderingHints.VALUE_INTERPOLATION_BILINEAR));
     }
 
-    public static BufferedImage prepare(BufferedImage image,
-                                        AffineTransform transform,
-                                        Map<RenderingHints.Key, ?> hints) {
-        BufferedImage result = image;
-        while (transform.getScaleX() < 0.5
-                || transform.getScaleY() < 0.5) {
-            result = scaleHalf(result, transform, hints);
-        }
-        return result;
-    }
+    public static BufferedImage resize(BufferedImage image,
+                                       int targetWidth,
+                                       int targetHeight,
+                                       Map<RenderingHints.Key, ?> hints) {
+        int sourceWidth = image.getWidth();
+        int sourceHeight = image.getHeight();
+        int halfWidth = (int) Math.ceil(sourceWidth / 2);
+        int halfHeight = (int) Math.ceil(sourceHeight / 2);
 
-    private static BufferedImage scaleHalf(BufferedImage image,
-                                           AffineTransform transform,
-                                           Map<RenderingHints.Key, ?> hints) {
-        int width = image.getWidth();
-        int height = image.getHeight();
-        double scaleX = 1;
-        double scaleY = 1;
-        if (transform.getScaleX() < 0.5) {
-            width = (int) (width * 0.5 + 0.5);
-            scaleX = image.getWidth() / (double) width;
+        BufferedImage source = image;
+        if (targetWidth < halfWidth
+                || targetHeight < halfHeight) {
+            int w = targetWidth < halfWidth ? targetWidth * 2 : targetWidth;
+            int h = targetHeight < halfHeight ? targetHeight * 2 : targetHeight;
+            source = resize(source, w, h, hints);
         }
-        if (transform.getScaleY() < 0.5) {
-            height = (int) (height * 0.5 + 0.5);
-            scaleY = image.getHeight() / (double) height;
-        }
-        transform.scale(scaleX, scaleY);
 
-        AffineTransform current = new AffineTransform();
-        current.scale(1 / scaleX, 1 / scaleY);
-
-        BufferedImage result = new BufferedImage(width, height,
-                                                 BufferedImage.TYPE_INT_ARGB);
-        Graphics2D g = result.createGraphics();
+        int imageType = source.getColorModel().hasAlpha()
+                        ? BufferedImage.TYPE_INT_ARGB
+                        : BufferedImage.TYPE_INT_RGB;
+        BufferedImage scaled =
+                new BufferedImage(targetWidth, targetHeight, imageType);
+        Graphics2D g = scaled.createGraphics();
         try {
             g.addRenderingHints(hints);
-            g.drawRenderedImage(image, current);
+            g.drawImage(source, 0, 0, targetWidth, targetHeight, null);
         } finally {
             g.dispose();
         }
-        return result;
+        return scaled;
     }
 
     private SmoothDownscale() {/* no instances */}

commit 598ea4555babdf3c2e9970adbcdff3c73354ad27

windows.Cursor: Crop or expand bitmap canvas to a target nominal size
Don't resample bitmaps in the windows.Cursor - this will be a feature of
the CompileCommand.  Just clamp input bitmaps to an explicit nominal size.

Historical note: It seems the canvasSize adjustment has been broken
since a828636899f436d4e403ef179cc7dee644f7467d "SmoothDownscale: Reverse scaling strategy".
This will be handled in CompileCommand, also.

mousegen/src/main/java/io/github/stanio/mousegen/BoxSizing.java
@@ -1,0 +1,71 @@
+/*
+ * SPDX-FileCopyrightText: 2023 Stanio <stanio AT yahoo DOT com>
+ * SPDX-License-Identifier: 0BSD
+ */
+package io.github.stanio.mousegen;
+
+import java.awt.Dimension;
+import java.awt.Rectangle;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Rectangle2D;
+
+/**
+ * Encapsulates target image dimension and a corresponding transformation
+ * for a given source image dimension.
+ */
+public class BoxSizing {
+
+    final Dimension target;
+    final AffineTransform transform;
+
+    /**
+     * Constructs a {@code BoxSizing} for target dimension equal to the
+     * given source dimension and an <i>identity</i> transformation.
+     *
+     * @param  source  source image dimension
+     */
+    public BoxSizing(Dimension source) {
+        this.target = new Dimension(source);
+        this.transform = new AffineTransform();
+    }
+
+    /**
+     * Constructs a {@code BoxSizing} with the given target dimension
+     * and a corresponding transformation from the given source dimension.
+     *
+     * @param  source  source image dimension
+     * @param  target  target image dimension
+     */
+    public BoxSizing(Dimension source, Dimension target) {
+        this(new Rectangle(source), target);
+    }
+
+    /**
+     * Constructs a {@code BoxSizing} with the given target dimension
+     * and a corresponding transformation from the given source view-box.
+     * <p>
+     * The view-box defines position and dimension within the source image
+     * to project into the given target dimension.  The view-box may specify
+     * dimension greater than the source image in which case the source
+     * canvas is expanded.  The primary use-case for this is for producing
+     * different cursor-scheme sizes (Regular, Large, Extra-Large) from a
+     * single source bitmap.</p>
+     *
+     * @param  viewBox  viewport position and dimension in source space
+     * @param  target  target image dimension
+     */
+    public BoxSizing(Rectangle2D viewBox, Dimension target) {
+        this.target = new Dimension(target);
+
+        AffineTransform txf = new AffineTransform();
+        txf.setToScale(target.width / viewBox.getWidth(),
+                       target.height / viewBox.getHeight());
+        txf.translate(-viewBox.getX(), -viewBox.getY());
+        this.transform = txf;
+    }
+
+    public AffineTransform getTransform() {
+        return new AffineTransform(transform);
+    }
+
+} // class BoxSizing

mousegen/src/main/java/io/github/stanio/mousegen/builder/providers/WindowsCursorFactory.java
@@ -80,7 +80,7 @@ class WindowsCursorBuilder extends CursorBuilder {
     @Override
     public void addFrame(Integer frameNo, int nominalSize, Point hotspot, BufferedImage image, int delayMillis) {
         frames.prepareFrame(validFrameNo(frameNo))
-                .addImage(image, hotspot);
+                .addImage(image, hotspot, nominalSize);
     }
 
     @Override

mousegen/src/main/java/io/github/stanio/mousegen/svg/SVGSizing.java
@@ -44,8 +44,7 @@ import java.awt.geom.Dimension2D;
 import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
 
-import io.github.stanio.windows.Cursor;
-
+import io.github.stanio.mousegen.BoxSizing;
 import io.github.stanio.mousegen.util.LocalXMLReader;
 import io.github.stanio.mousegen.util.SAXReplayBuffer;
 
@@ -248,7 +247,7 @@ public class SVGSizing {
             });
 
             Point2D hotspot = metadata.hotspot.pointWithOffset(strokeOffset, fillOffset);
-            Point2D offsetHotspot = new Cursor.BoxSizing(viewBox, targetDimension)
+            Point2D offsetHotspot = new BoxSizing(viewBox, targetDimension)
                                     .getTransform().transform(hotspot, null);
             int x = roundHotspotCoord(offsetHotspot.getX(), metadata.hotspot.bias().dX(),
                         metadata.hotspot.x() / metadata.sourceViewBox.getWidth());

src/main/java/io/github/stanio/windows/Cursor.java
@@ -24,16 +24,13 @@ import java.util.List;
 import java.util.logging.Logger;
 
 import java.awt.Dimension;
+import java.awt.Graphics2D;
 import java.awt.Point;
-import java.awt.Rectangle;
-import java.awt.geom.AffineTransform;
 import java.awt.geom.Point2D;
-import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
 
 import javax.imageio.ImageIO;
 
-import io.github.stanio.awt.SmoothDownscale;
 import io.github.stanio.io.BufferedChannelOutput;
 import io.github.stanio.io.DataFormatException;
 import io.github.stanio.io.ReadableChannelBuffer;
@@ -53,68 +50,6 @@ import io.github.stanio.windows.CursorReader.DirEntry;
  * @see  CursorReader
  */
 public class Cursor {
-
-
-    /**
-     * Encapsulates target image dimension and a corresponding transformation
-     * for a given source image dimension.
-     */
-    public static class BoxSizing {
-
-        final Dimension target;
-        final AffineTransform transform;
-
-        /**
-         * Constructs a {@code BoxSizing} for target dimension equal to the
-         * given source dimension and an <i>identity</i> transformation.
-         *
-         * @param  source  source image dimension
-         */
-        public BoxSizing(Dimension source) {
-            this.target = new Dimension(source);
-            this.transform = new AffineTransform();
-        }
-
-        /**
-         * Constructs a {@code BoxSizing} with the given target dimension
-         * and a corresponding transformation from the given source dimension.
-         *
-         * @param  source  source image dimension
-         * @param  target  target image dimension
-         */
-        public BoxSizing(Dimension source, Dimension target) {
-            this(new Rectangle(source), target);
-        }
-
-        /**
-         * Constructs a {@code BoxSizing} with the given target dimension
-         * and a corresponding transformation from the given source view-box.
-         * <p>
-         * The view-box defines position and dimension within the source image
-         * to project into the given target dimension.  The view-box may specify
-         * dimension greater than the source image in which case the source
-         * canvas is expanded.  The primary use-case for this is for producing
-         * different cursor-scheme sizes (Regular, Large, Extra-Large) from a
-         * single source bitmap.</p>
-         *
-         * @param  viewBox  viewport position and dimension in source space
-         * @param  target  target image dimension
-         */
-        public BoxSizing(Rectangle2D viewBox, Dimension target) {
-            this.target = new Dimension(target);
-
-            AffineTransform txf = new AffineTransform();
-            txf.setToScale(target.width / viewBox.getWidth(),
-                           target.height / viewBox.getHeight());
-            txf.translate(-viewBox.getX(), -viewBox.getY());
-            this.transform = txf;
-        }
-
-        public AffineTransform getTransform() {
-            return new AffineTransform(transform);
-        }
-
-    } // class BoxSizing
 
 
     static final class Image {
@@ -288,46 +223,42 @@ public class Cursor {
     /**
      * Adds a variant image to this cursor.  This is equivalent to:
      * <pre>
-     * <code>    BoxSizing originalSize = new BoxSizing(
-     *             new Dimension(image.getWidth(), image.getHeight()));
-     *     addImage(image, hotspot, originalSize);</code></pre>
+     * <code>    addImage(image, hotspot, image.getWidth());</code></pre>
      *
      * @param   image  variant image to add
      * @param   hotspot  hotspot for the given variant image
-     * @see     #addImage(BufferedImage, Point2D, BoxSizing)
+     * @see     #addImage(BufferedImage, Point2D, int)
      */
     public void addImage(BufferedImage image, Point2D hotspot) {
-        addImage(image, hotspot, new BoxSizing(imageSize(image)));
+        addImage(image, hotspot, image.getWidth());
     }
 
     /**
-     * Adds a variant image to this cursor.  The given {@code sizing} may
-     * represent an <i>identity</i> transform in which case the source image
-     * and hotspot are used as given.  The given {@code hotspot} is always
-     * interpreted in the source image coordinates, and is adjusted according
-     * to the given {@code sizing} as necessary.
+     * Adds a variant image to this cursor.  If the given image dimensions
+     * don't match the given {@code nominalSize}, the bitmap will be cropped or
+     * expanded as necessary.
      *
      * @param   image  variant image to add
      * @param   hotspot  hotspot for the given variant image
-     * @param   sizing  target size transformation to apply to the given
-     *          source image and hotspot
+     * @param   nominalSize  target canvas size
      */
-    public void addImage(BufferedImage image, Point2D hotspot, BoxSizing sizing) {
+    public void addImage(BufferedImage image, Point2D hotspot, int nominalSize) {
         if (entries.size() >= 0xFFFF) // DWORD
             throw new IllegalStateException("Too many images: " + entries.size());
 
         BufferedImage argb;
-        Point hxy;
-        if (sizing.transform.isIdentity()
-                && sizing.target.width == image.getWidth()
-                && sizing.target.height == image.getHeight()
+        Point hxy = clampHotspot(hotspot);
+        if (image.getWidth() == nominalSize
+                && image.getHeight() == nominalSize
                 && isARGB(image)) {
             argb = image;
-            hxy = clampHotspot(hotspot);
         } else {
-            argb = SmoothDownscale.resize(image,
-                    sizing.target.width, sizing.target.height);
-            hxy = clampHotspot(sizing.transform.transform(hotspot, null));
+            BufferedImage canvas = new BufferedImage(nominalSize, nominalSize,
+                                                     BufferedImage.TYPE_INT_ARGB);
+            Graphics2D g = canvas.createGraphics();
+            g.drawImage(image, 0, 0, null);
+            g.dispose();
+            argb = canvas;
         }
         addARGBImage(argb, hxy);
     }
@@ -398,24 +329,10 @@ public class Cursor {
      * @param   file  file to read the variant image from
      * @param   hotspot  hotspot for the given variant image
      * @throws  IOException  if I/O error or failure to decode the image happens
-     * @see     #addImage(Path, Point2D, BoxSizing)
+     * @see     #addImage(Path, Point2D)
      */
     public void addImage(Path file, Point2D hotspot) throws IOException {
         addImage(loadImage(file), hotspot);
-    }
-
-    /**
-     * Adds a variant image to this cursor.
-     *
-     * @param   file  file to read the variant image from
-     * @param   hotspot  hotspot for the given variant image
-     * @param   sizing  sizing transformation to apply to the given
-     *          source image and hotspot
-     * @throws  IOException  if I/O error or failure to decode the image happens
-     * @see     #addImage(BufferedImage, Point2D, BoxSizing)
-     */
-    public void addImage(Path file, Point2D hotspot, BoxSizing sizing) throws IOException {
-        addImage(loadImage(file), hotspot, sizing);
     }
 
     /**